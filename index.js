6eb14b7e5e55a14201ee7f9428399a8fece2e4f9
Travis You <1244098000@qq.com>
2022-06-07 14:34:07 +0800

    docs: update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 var a = '[a, b, c, d]';
 var b = [1, 2, 3, 4];
 //TODO
+console.log("-------------- Answer 1 --------------")
 console.log("Whether a is an Array?: ", Array.isArray(a));
 console.log("Whether b is an Array?: ", Array.isArray(b));
 
@@ -11,6 +12,7 @@ var a = [1, 2, 3, 4, 5];
 for (index in a) {
     a[index] *= 2;
 }
+console.log("\n-------------- Answer 2 --------------")
 console.log("The output of program:  ", a);
 
 // Write the program, according to the following requirements output results.
@@ -18,6 +20,7 @@ var colors = ["Red", "Green", "White", "Black"];
 // TODO case 1 output: 'Red Green White Black'
 // case 2 output: 'Red+Green+White+Black'
 // case 3 output: 'Red,Green,White,Black'
+console.log("\n-------------- Answer 3 --------------")
 console.log("The output of case1:    ", colors.join(" "))
 console.log("The output of case2:    ", colors.join("+"))
 console.log("The output of case3:    ", colors.join(","))
@@ -26,6 +29,7 @@ console.log("The output of case3:    ", colors.join(","))
 // Write a program to sort the Numbers in the following array from largest to smallest.
 var a = [5, 1, 8, 10, 4];
 //TODO should output: [10,8,5,4,1]
+console.log("\n-------------- Answer 4 --------------")
 console.log("The output of sort a:   ", a.sort(function(a, b) { return b - a }));
 
 // Program to find the most frequent element in the following array.
@@ -39,4 +43,5 @@ a.forEach(element => {
     map.set(element, cnt);
     if (cnt > max_cnt) ans = element, max_cnt = cnt;
 });
+console.log("\n-------------- Answer 5 --------------")
 console.log("The output of search:   ", ans);
